107199a2150fe185abc60e575c854d4e3eac275f
Veronica Carter <42939810+vermerker@users.noreply.github.com>
2020-08-29 22:37:39 +0100

    set tweet interval to 7.5-15hrs

---
+++ bee.js
@@ -0,0 +1,59 @@
+
+const fs = require( 'fs' ),
+      path = require( 'path' ),
+      Twit = require( 'twit' ),
+      config = require( path.join( __dirname, 'config.js' ) );
+
+const T = new Twit(config);
+
+// every 7.5 - 15hrs
+
+setInterval(tweetIt, 
+    
+    (Math.floor(Math.random() * (10 - 5 + 1)) + 5) * 15000 * 60 * 6);
+
+
+
+function tweetIt() {
+      T.post( 'statuses/update', { status: newhive()
+    
+      }, function( err, data, response ) {
+            console.log( data )
+      } );
+}
+
+function newhive() {
+
+    const weather = ['. .','...','...','. .','☁️', '❄️','🌧️', '🌩️', '🌨️', '⛅', '☀️','⛈️'];
+    const wax = ['🟨','🔶','🟧','🛑',' ⬡ ',' ⬡ ','🐝','🐝','🐝'];
+    const bugs = ['へ','へ','へ','へ','へ','へ','へ','🐛','🐜','🦋','🌿','🌿','🌿','🌿','🌿','💐','🍀','🌻'];
+
+
+    let bugShuffled = bugs.sort(() => 0.5 - Math.random());
+    let selectedbugs = bugShuffled.slice(0, 8).join('');
+
+    let weatherShuffled = weather.sort(() => 0.5 - Math.random());
+    let selectedWeather = weatherShuffled.slice(0, 6).join('');
+
+    function bees(){
+        let beeLine = wax.sort(() => 0.5 - Math.random());
+        return beeLine.slice(0, 5).join('');
+    }
+
+    console.log("it worked!");
+
+    return   '....' + selectedWeather + '....\n' +
+             ".  ___________________\n" +
+             "╱| ________________ ╱|\n" +
+             '| |'+ bees() + '| |\n' + 
+             '| |'+ bees() + '| |\n' + 
+             '| |'+ bees() + '| |\n' + 
+             "|╱ ⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴|╱\n" +
+             " ￣￣￣￣￣￣￣￣\n" +
+             selectedbugs;
+
+
+
+
+
+}
